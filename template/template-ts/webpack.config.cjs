9067c431437a4a5bcb81d24edd190c2a3851c96f
wangyi <vwangyi@foxmail.com>
2026-09-26 06:57:31 +0800

    feat(root): mode

--- template/template-ts/webpack.config.cjs
+++ template/template-ts/webpack.config.cjs
@@ -1,32 +1,34 @@
-const { defineConfig } = require('webpack');
+const webpack = require('webpack');
 const path = require('node:path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = defineConfig({
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts/,
-        use: ['ts-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './public/index.html')
-    })
-  ],
-  devServer: {
-    port: 1236,
-    open: true
-  }
-});
+module.exports = (_, { mode }) => {
+  return {
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts/,
+          use: ['ts-loader']
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './public/index.html')
+      })
+    ],
+    devServer: {
+      port: 1236,
+      open: true
+    }
+  };
+};
